3cd010af3a95099f2c0b4fd593b1981929f61c1d
Kwon ohHyun <mmdkansk@gmail.com>
2017-04-04 18:35:48 +0900

    Add testcode

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -14,5 +14,74 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int output = calc.calculate("1+1");
         assertThat(output, is(2));
+    } 
+    
+    public void testCalculatorSub () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("3-4");
+	assertThat(output, is(-1));
     }
-}
+
+    public void testCalculatorMul () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("5*7");
+	assertThat(output, is(35));
+    }
+
+    public void testCalculatorDiv () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("30/5");
+	assertThat(output, is(6));
+    }
+
+    public void testCalculatorCom1 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("(5-3)*6");
+	assertThat(output, is(12));
+    }
+
+    public void testCalculatorCom2 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("((3-7)*(6/2))*(2-4)");
+	assertThat(output, is(24));
+    }
+
+    public void testCalculatorCom3 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("5*((9/3)-10)");
+	assertThat(output, is(-35));
+    }
+
+    public void testCalculatorCom4 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("4+3*5");
+	assertThat(output, is(19));
+    }
+
+    public void testCalculatorCom5 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("((10*2-(2+4))*(5-4))/2");
+	assertThat(output, is(7));
+    }
+
+    public void testCalculatorCom6 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("(((3-6)*2)+8)/2");
+	assertThat(output, is(1));
+    }
+
+    public void testCalculatorCom7 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("10/5*2");
+	assertThat(output, is(4));
+    }
+
+    public void testCalculatorCom8 () {
+	Calculator calc = new Calculator();
+	int output = calc.calculate("(((200/10)/(156-151))+((645+35)-(67*10)))");
+	assertThat(output, is(14));
+    }
+
+
+
+
