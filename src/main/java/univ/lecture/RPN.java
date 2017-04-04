0b18435a86e818caece3bd5f873cfeb4366d8842
Chihyeon Ryu <rch0218@gmail.com>
2017-04-04 23:28:38 +0900

    Modify RPN.java

--- src/main/java/univ/lecture/RPN.java
+++ src/main/java/univ/lecture/RPN.java
@@ -4,102 +4,111 @@ import java.util.*;
 
 public class RPN{
 
-	private static boolean isAnOperator(char s) {
-		return (s == '+' || s == '-' || s == '*' || s == '/');
+	private static boolean isAnOperator(String s) {
+		return (s.length() == 1 && "+-*/".indexOf(s) >= 0);
 	}
 
-	private static int evaluate(int x, int y, char op) {
-		int z = 0;
-		if (op == '+')
+	private static double evaluate(double x, double y, String op) {
+		double z = 0;
+
+		if (op.equals("+"))
 			z = x + y;
-		else if (op == '-')
+		else if (op.equals("-"))
 			z = x - y;
-		else if (op == '*')
+		else if (op.equals("*"))
 			z = x * y;
 		else
 			z = x / y;
+		
 		return z;
 	}
 
 	public static int calculateExpression(String exp){
-		return calculatePostFix(infixToPostfix(exp));
+		return calculatePostFix(infixToPostfix(exp);
 	}
 
-	private static int calculatePostFix(String postfix){
-
+	private static double calculatePostFix(String[] args){
 		Stack<Object> stack = new Stack<Object>();
-		for (int i = 0; i < postfix.length(); i++) {
-			char input = postfix.charAt(i);
-			int input_int = 0;
-				
-			if (isAnOperator(input)) {
-				int y = (int)stack.pop();
-				int x = (int)stack.pop();
-				int z = evaluate(x, y, input);
-				stack.push(z);
-			}
-			else{
-				input_int = Character.getNumericValue(input); 
-				stack.push(input_int);
-			}
-		}
-		return 0;
-	}
 		
-	private static String infixToPostfix(String infix){
-		String postfix = "";
-		Stack<Object> stack = new Stack<Object>();
+		for (int i = 0; i < args.length; i++) {
+			String input = args[i];
 
-		for(int i = 0; i < infix.length(); i++){ 
-			char c = infix.charAt(i);
-			if(c >= '0' && c <= '9'){ 
-				postfix += c;
-			}
-			else if (c == '+' || c == '-') { 
-				while(!stack.isEmpty()) { 
-					
-					char prev = (char)stack.pop();
-					if ( prev == '*' || prev == '/' || prev == '+' || prev == '-') {
-						postfix += prev; 	  
-					}						
-					else {
-						stack.push(prev);	
-						break;
-					}
-				}
-				stack.push(c);             
-			}
-			else if (c == '*' || c == '/') { 
-				while(!stack.isEmpty()) { 
-					
-					char prev = (char)stack.pop();
-					if(prev == '*' || prev == '/'){ 
-						postfix += prev;			
-					}
-					else
-					{								
-						stack.push(prev);			
-						break;
-					}
-				}
-				stack.push(c);	
-			}
-			else if ( c == '(') { 
-				stack.push(c);
-			}
-			else if ( c == ')') { 
-				while(true){      
-					char prev = (char)stack.pop();
-					if( prev == '(')
-						break;
-					postfix += prev;
-				}
-			}
+			if (isAnOperator(input)) {
+				double y = Double.parseDouble((String) stack.pop());
+				double x = Double.parseDouble((String) stack.pop());
+				double z = evaluate(x, y, input);
+				stack.push("" + z);
+			} else
+				stack.push(input);
 		}
-		while( !stack.isEmpty() ){ 
-			postfix += stack.pop();
-		}
-		return "";
+		
+		return stack.pop();
 	}
+	
+	private static String[] infixToPostfix(String[] args){
+		Stack<Object> stack = new Stack<Object>();
+		
+		String tempPostfix = new String();
+		int count = 0;
+		String[] changedArgs = args[0].split("(?<=[\\(\\)\\+\\-*\\/\\^A-Za-z])|(?=[\\(\\)\\+\\-*\\/\\^A-Za-z])");
+		
+		for (int i = 0; i < changedArgs.length; i++) {
+			if (precedence(changedArgs[i]) == 3) {
+				stack.push(changedArgs[i]);
+			} else if (precedence(changedArgs[i]) == 9) {
+				while (!stack.peek().equals("(")) {
+					tempPostfix += (String) stack.pop() + " ";
+					count++;
+				}
+				stack.pop();
+			} else if (precedence(changedArgs[i]) == 0) {
+				tempPostfix += changedArgs[i] + " ";
+				count++;
+			} else if (!stack.isEmpty() && (precedence(changedArgs[i]) <= precedence((String) stack.peek()))) {
+				while (!stack.isEmpty() && (precedence(changedArgs[i]) <= precedence((String) stack.peek()))) {
+					tempPostfix += (String) stack.pop() + " ";
+					count++;
+				}
+				stack.push(changedArgs[i]);
+			} else
+				stack.push(changedArgs[i]);
+		}
 
-}
+		while (!stack.isEmpty()) {
+			tempPostfix += (String) stack.pop() + " ";
+			count++;
+		}
+
+		StringTokenizer parser = new StringTokenizer(tempPostfix, " ");
+		String[] postfix = new String[count];
+		int j = 0;
+
+		while (parser.hasMoreTokens()) {
+			postfix[j] = parser.nextToken();
+			j++;
+		}
+
+		System.out.print("Chaned Postfix : ");
+		for (int k = 0; k < postfix.length; k++)
+			System.out.print(postfix[k] + " ");
+		System.out.println();
+
+		return postfix;
+	}
+	
+	private static int precedence(String token) {
+		if (token.equals("+")) {
+			return 5;
+		} else if (token.equals("-")) {
+			return 5;
+		} else if (token.equals("/")) {
+			return 7;
+		} else if (token.equals("*")) {
+			return 7;
+		} else if (token.equals("(")) {
+			return 3;
+		} else if (token.equals(")")) {
+			return 9;
+		} else
+			return 0;
+	}
